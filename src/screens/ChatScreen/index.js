f4111d274e21f0f15ccfc2b34facf94286f6a4b1
Fırat Sezel <firat.sezel@gmail.com>
2020-07-29 10:51:01 +0300

    chat page initialize

--- src/screens/ChatScreen/index.js
+++ src/screens/ChatScreen/index.js
@@ -1,13 +1,54 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { AutoGrowingTextInput } from 'react-native-autogrow-textinput';
+import firebase from '@react-native-firebase/app';
+import database from '@react-native-firebase/database';
 import styles from './styles';
 
 export default function ChatScreen({ route, navigation }) {
+  const [text, setText] = useState('');
+  const [conversation, setConversation] = useState([]);
+
+  const getInfo = async () => {
+      const { roomName, person, userName } = route.params;
+      firebase.database().ref(`/users/${userName}/${roomName}`).once('value')
+      .then(snapshot => {
+          console.log('snapshot.val()', snapshot.val());
+
+          if (snapshot.val().conversation) { // daha önce yapılmış bir konuşma var
+            setConversation(snapshot.val().conversation);
+          } else { // yeni açılan bir chat
+            
+          }
+      });
+  }
+
+  useEffect(() => {getInfo()}, []);
+
   return (
     <View style={styles.container}>
-      <Text>Chat Screen</Text>
+      <KeyboardAvoidingView style={{ flex: 1, backgroundColor: '#eee' }} behavior="padding">
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'position' : null} style={[styles.container, { position: 'absolute' }]}>
+          {/*this.renderList()*/}
+          <View style={styles.textBox}>
+            <AutoGrowingTextInput
+              autoCorrect={false}
+              value={text}
+              style={styles.input}
+              underlineColorAndroid="transparent"
+              placeholder="Type something nice"
+              onChangeText={(newtxt) => {
+                setText(newtxt);
+              }}
+            />
+            <TouchableOpacity onPress={() => {}}>
+              <Text style={styles.send}>Send</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </KeyboardAvoidingView>
     </View>
   );
 }
--- src/screens/ChatScreen/styles.js
+++ src/screens/ChatScreen/styles.js
@@ -1,9 +1,108 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+const screenWidth = Dimensions.get('window').width; //full screen width
+const screenHeight = Dimensions.get('window').height; //full screen width
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: 0,
+  },
+  panelContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  newInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    fontSize: 16,
+    padding: 10,
+    height: 50,
+  },
+  panel: {
+    flexDirection: 'column',
+    height: screenHeight,
+    paddingVertical: 7,
+    paddingHorizontal: 7,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    // backgroundColor: '#e6f2ff',
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 5,
+    shadowOpacity: 0.4,
+  },
+  send: {
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    padding: 20,
+  },
+  input: {
+    alignSelf: 'center',
+    paddingHorizontal: 15,
+    fontSize: 14,
+    flex: 1,
+  },
+  /* footer: {
+    flexDirection: 'row',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    backgroundColor: '#b78b9e',
+    height: 40,
+  }, */
+  newInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    fontSize: 16,
+    padding: 10,
+    height: 50,
+  },
+  lightning: {
+    fontSize: 26,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    width: screenWidth,
+    height: 40,
+  },
+  textBox: {
+    flexDirection: 'row',
+    //bottom: 0,
+    backgroundColor: '#ccc', // b78b9e
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  textInput: {
+    width: screenWidth / 1.25,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#9ab',
+    height: 30,
+    marginTop: 3,
+    bottom: 3,
+    marginLeft: 5,
+    marginRight: 10,
+  },
+  listView: {
+    transform: [{ scaleY: -1 }],
+    width: screenWidth,
+    height: screenHeight - 60,
+  },
+  sentenceListView: {
+    //backgroundColor: '#e6f2ff',
+    flexWrap: 'wrap',
+    bottom: 30,
+    position: 'absolute',
+    width: screenWidth,
+    maxHeight: 500,
+    marginBottom: 0,
+  },
 });
-
-module.exports = styles;
